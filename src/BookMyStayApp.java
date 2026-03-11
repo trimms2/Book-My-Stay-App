3dd5e4237c755b9fbd5612a91fe40f6d126645ff
trimman <trimmankoduri@gmail.com>
2026-03-11 10:23:57 +0530

    Use Case-3 Implemented room inventory and suit room functionality

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,19 +1,29 @@
+import java.util.Map;
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
+        System.out.println("===== Book My Stay App =====");
+        System.out.println("Hotel Room Inventory Status\n");
+
         Room single = new SingleRoom();
         Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
+        RoomInventory inventory = new RoomInventory();
+        Map<String, Integer> availability = inventory.getRoomAvailability();
 
-        System.out.println("Single Room Details");
+        System.out.println("Single Room:");
         single.displayRoomDetails();
-        System.out.println("Available: " + singleAvailable);
+        System.out.println("Available Rooms: " + availability.get("Single"));
 
-        System.out.println("\nDouble Room Details");
+        System.out.println("\nDouble Room:");
         doubleRoom.displayRoomDetails();
-        System.out.println("Available: " + doubleAvailable);
+        System.out.println("Available Rooms: " + availability.get("Double"));
+
+        System.out.println("\nSuite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available Rooms: " + availability.get("Suite"));
     }
 }
